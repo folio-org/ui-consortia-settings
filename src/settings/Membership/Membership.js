0e0dcebffbb1bb3585ff66bf202daa72aafd81c1
Yury Saukou <yury_saukou@epam.com>
2023-03-10 18:33:09 +0400

    UICONSET-2 UICONSET-3 Add controlled vocabulary component to render consortium members (#4)
    * UIOR-1081 UIOR-1082 Add controlled vocabulary component to render consortium members
    
    * UICONSET-1 UICONSET-2 UICONSET-3 update Jira project info
    
    * resolve review comment

--- src/settings/Membership/Membership.js
+++ src/settings/Membership/Membership.js
@@ -1,5 +1,71 @@
+import { noop } from 'lodash';
+import { FormattedMessage, useIntl } from 'react-intl';
+
+import { ControlledVocab } from '@folio/stripes/smart-components';
+import { getControlledVocabTranslations } from '@folio/stripes-acq-components';
+
+import { tenants } from '../../../test/jest/fixtures/tenants';
+
+/* TODO: replace mocks with actual resources during connecting with BE */
+const mockedStripes = {
+  connect: noop,
+  hasPerm: noop,
+};
+
+const mockMutator = {
+  values: {
+    POST: noop,
+  },
+  updaterIds: {
+    replace: noop,
+  },
+};
+
+const mockResources = {
+  values: { records: tenants },
+};
+
+const actionProps = {
+  edit: () => ({ disabled: true }),
+};
+
+const suppressEdit = () => false;
+/* ^^^ */
+
+const columnMapping = {
+  tenantName: <FormattedMessage id="ui-consortia-settings.settings.membership.list.tenantName" />,
+  tenantId: <FormattedMessage id="ui-consortia-settings.settings.membership.list.tenantAddress" />,
+};
+const hiddenFields = ['numberOfObjects', 'lastUpdated'];
+const visibleFields = ['tenantName', 'tenantId'];
+
+const suppressDelete = () => true;
+const actionSuppressor = { edit: suppressEdit, delete: suppressDelete };
+
 export const Membership = () => {
+  const intl = useIntl();
+
   return (
-    <div>Membership settings</div>
+    <ControlledVocab
+      id="consortia-membership"
+      actionProps={actionProps}
+      actionSuppressor={actionSuppressor}
+      canCreate={false}
+      formType="final-form"
+      stripes={mockedStripes}
+      resources={mockResources}
+      readOnlyFields={['tenantId']}
+      mutator={mockMutator}
+      // TODO: replace according to the contract during connecting with BE
+      baseUrl="<REFINE endpoint>"
+      records="<REFINE fieldName>"
+      // ^^^
+      label={intl.formatMessage({ id: 'ui-consortia-settings.settings.membership.heading' })}
+      objectLabel={intl.formatMessage({ id: 'ui-consortia-settings.settings.membership.objectLabel' })}
+      translations={getControlledVocabTranslations('ui-consortia-settings.settings.membership.list')}
+      columnMapping={columnMapping}
+      hiddenFields={hiddenFields}
+      visibleFields={visibleFields}
+    />
   );
 };
---
+++ test/jest/fixtures/tenants.js
@@ -0,0 +1,12 @@
+export const tenants = [
+  { tenantName: 'East Central College', tenantId: 'f00123454235' },
+  { tenantName: 'Jefferson College', tenantId: 'f001234009965' },
+  { tenantName: 'Mineral Area College', tenantId: 'f001234252634' },
+  { tenantName: 'St. Charles Community College', tenantId: 'f001234009912' },
+  { tenantName: 'St. Louis Community College', tenantId: 'f001234154456' },
+  { tenantName: 'Three Rivers College', tenantId: 'f001234009923' },
+  { tenantName: 'Columbia College', tenantId: 'f001277776665' },
+  { tenantName: 'Lincoln University', tenantId: 'f001234009933' },
+  { tenantName: 'Stephens College', tenantId: 'f001231234444' },
+  { tenantName: 'Missouri State Library', tenantId: 'f001234009956' },
+];
