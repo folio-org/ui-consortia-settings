749fa856a577b589a15b50d5838720385d2e7811
Alisher Musurmonov <alisher_musurmonov1@epam.com>
2023-04-26 17:35:50 +0500

    feat: add unique code field for consortium settings

--- src/settings/Membership/validate.js
+++ src/settings/Membership/validate.js
@@ -1,6 +1,6 @@
 import { FormattedMessage } from 'react-intl';
 
-import { MAX_NAME_LENGTH } from './constants';
+import { MAX_NAME_LENGTH, MAX_CODE_LENGTH } from './constants';
 
 const validateMaxNameLength = (name = '') => (
   name.length > MAX_NAME_LENGTH && (
@@ -11,20 +11,42 @@ const validateMaxNameLength = (name = '') => (
   )
 );
 
+const validateMaxCodeLength = (code = '') => (
+  code.length > MAX_CODE_LENGTH && (
+    <FormattedMessage
+      id="ui-consortia-settings.settings.membership.error.codeExceedsLength"
+      values={{ count: MAX_CODE_LENGTH }}
+    />
+  )
+);
+
 const validateNameUniqueness = (name, i, items = []) => (
   items.some(({ name: _name }, _i) => (name === _name && i !== _i)) && (
     <FormattedMessage id="ui-consortia-settings.settings.membership.error.duplicate" />
   )
 );
 
-export const validate = ({ name }, i, items) => {
+const validateCodeUniqueness = (code, i, items = []) => (
+  items.some(({ code: _code }, _i) => (code === _code && i !== _i)) && (
+    <FormattedMessage id="ui-consortia-settings.settings.membership.error.duplicate.code" />
+  )
+);
+
+export const validate = ({ name, code }, i, items) => {
   const nameError = (
     validateMaxNameLength(name)
     || validateNameUniqueness(name, i, items)
     || undefined
   );
 
+  const codeError = (
+    validateMaxCodeLength(code)
+    || validateCodeUniqueness(code, i, items)
+    || undefined
+  );
+
   return {
     name: nameError,
+    code: codeError,
   };
 };
