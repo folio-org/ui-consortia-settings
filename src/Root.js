d8879c864de3156d875950d8dbc225720597ca9d
Yury Saukou <yury_saukou@epam.com>
2023-05-23 20:12:13 +0400

    UICONSET-7 move module event handler to the Root component

--- src/Root.js
+++ src/Root.js
@@ -1,33 +1,46 @@
 import PropTypes from 'prop-types';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { MODULE_ROOT_ROUTE } from './constants';
+import { eventHandler } from './eventHandler';
 import { ConsortiumManager } from './routes';
 import ConsortiumSettings from './settings';
+import { checkConsortiumAffiliations } from './utils';
 
-const Root = ({
-  location,
-  match,
-  showSettings,
-}) => {
-  if (showSettings) {
+class Root extends React.Component {
+  // Checks whether to show "Switch active affiliation" action item in the profile dropdown (package.json::stripes.links.userDropdown[0])
+  static checkConsortiumAffiliations = checkConsortiumAffiliations;
+
+  // Implementation of the handler specified in package.json ("stripes.handlerName")
+  static eventHandler = eventHandler;
+
+  render() {
+    const {
+      location,
+      match,
+      showSettings,
+    } = this.props;
+
+    if (showSettings) {
+      return (
+        <ConsortiumSettings
+          location={location}
+          match={match}
+        />
+      );
+    }
+
     return (
-      <ConsortiumSettings
-        location={location}
-        match={match}
-      />
+      <Switch>
+        <Route
+          path={MODULE_ROOT_ROUTE}
+          component={ConsortiumManager}
+        />
+      </Switch>
     );
   }
-
-  return (
-    <Switch>
-      <Route
-        path={MODULE_ROOT_ROUTE}
-        component={ConsortiumManager}
-      />
-    </Switch>
-  );
-};
+}
 
 Root.propTypes = {
   location: PropTypes.shape({
