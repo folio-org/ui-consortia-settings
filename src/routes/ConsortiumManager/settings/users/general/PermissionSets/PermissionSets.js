952b5a1b422ac74364b8312d7e2eaa97866c2c38
Yury Saukou <yury_saukou@epam.com>
2023-06-19 13:55:03 +0400

    UICONSET-58 add error handling

--- src/routes/ConsortiumManager/settings/users/general/PermissionSets/PermissionSets.js
+++ src/routes/ConsortiumManager/settings/users/general/PermissionSets/PermissionSets.js
@@ -1,15 +1,12 @@
 import ReactRouterPropTypes from 'react-router-prop-types';
 import { noop } from 'lodash';
 import { useCallback, useMemo, useState } from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
-import {
-  Selection,
-} from '@folio/stripes/components';
+import { Selection } from '@folio/stripes/components';
 import { EntrySelector } from '@folio/stripes/smart-components';
-import {
-  PermissionSetDetails,
-} from '@folio/users';
+import { useShowCallout } from '@folio/stripes-acq-components';
+import { PermissionSetDetails } from '@folio/users';
 
 import { useTenantPermissions } from '../../../../../../hooks';
 import { UUID_REGEX } from '../../../../../../constants';
@@ -22,6 +19,10 @@ const nameKey = 'displayName';
 
 export const PermissionSets = (props) => {
   const { history, location, match } = props;
+
+  const intl = useIntl();
+  const showCallout = useShowCallout();
+
   const {
     activeMember,
     membersOptions,
@@ -31,16 +32,35 @@ export const PermissionSets = (props) => {
     new RegExp(UUID_REGEX).exec(location.pathname)?.[0]
   ));
 
+  const handleLogsLoadingError = useCallback(({ response }) => {
+    const defaultMessage = intl.formatMessage({ id: 'ui-consortia-settings.errors.permissionSets.load.common' });
+
+    if (response?.status === 403) {
+      return showCallout({
+        message: `${defaultMessage} ${intl.formatMessage({ id: 'ui-consortia-settings.errors.permissionsRequired' })}`,
+        type: 'error',
+      });
+    }
+
+    return showCallout({
+      message: defaultMessage,
+      type: 'error',
+    });
+  }, [intl, showCallout]);
+
   const {
     isFetching,
     permissions: contentData,
-  } = useTenantPermissions({
-    tenantId: activeMember,
-    searchParams: {
-      query: 'mutable==true',
-      expandSubs: true,
+  } = useTenantPermissions(
+    {
+      tenantId: activeMember,
+      searchParams: {
+        query: 'mutable==true',
+        expandSubs: true,
+      },
     },
-  });
+    { onError: handleLogsLoadingError },
+  );
 
   const onMemberChange = useCallback((member) => {
     setActiveMember(member);
