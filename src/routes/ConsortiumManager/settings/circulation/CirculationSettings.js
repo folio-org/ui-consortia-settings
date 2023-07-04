3a85c7101de6df50dc3782ccacc394ba2a35ffd4
Yury Saukou <yury_saukou@epam.com>
2023-07-04 12:59:49 +0400

    UICONSET-130 Add request cancellation reasons settings (circulation)

--- src/routes/ConsortiumManager/settings/circulation/CirculationSettings.js
+++ src/routes/ConsortiumManager/settings/circulation/CirculationSettings.js
@@ -3,11 +3,27 @@ import { FormattedMessage } from 'react-intl';
 import { stripesShape } from '@folio/stripes/core';
 import { Settings } from '@folio/stripes/smart-components';
 
+import { RequestCancellationReasons } from './requests';
+
+const sections = [
+  {
+    label: <FormattedMessage id="ui-circulation.settings.index.request" />,
+    pages: [
+      {
+        route: 'cancellation-reasons',
+        label: <FormattedMessage id="ui-circulation.settings.index.requestCancellationReasons" />,
+        component: RequestCancellationReasons,
+        perm: 'ui-circulation.settings.cancellation-reasons',
+      },
+    ],
+  },
+];
+
 const CirculationSettings = (props) => {
   return (
     <Settings
       {...props}
-      sections={[]}
+      sections={sections}
       paneTitle={<FormattedMessage id="ui-circulation.settings.index.paneTitle" />}
     />
   );
