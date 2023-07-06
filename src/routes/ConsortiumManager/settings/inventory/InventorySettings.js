eaa0b16651fa2f2ee04866a5ab8ee41dfd82eb7e
Yury Saukou <yury_saukou@epam.com>
2023-07-06 16:10:35 +0400

    UICONSET-130 update routes values

--- src/routes/ConsortiumManager/settings/inventory/InventorySettings.js
+++ src/routes/ConsortiumManager/settings/inventory/InventorySettings.js
@@ -35,19 +35,19 @@ const sections = [
     label: <FormattedMessage id="ui-inventory.instances" />,
     pages: [
       {
-        route: 'alternativeTitleTypes',
+        route: 'alternative-title-types',
         label: <FormattedMessage id="ui-inventory.alternativeTitleTypes" />,
         component: AlternativeTitleTypes,
         perm: 'ui-inventory.settings.alternative-title-types',
       },
       {
-        route: 'classificationTypes',
+        route: 'classification-types',
         label: <FormattedMessage id="ui-inventory.classificationIdentifierTypes" />,
         component: ClassificationTypes,
         perm: 'ui-inventory.settings.classification-types',
       },
       {
-        route: 'contributortypes',
+        route: 'contributor-types',
         label: <FormattedMessage id="ui-inventory.contributorTypes" />,
         component: ContributorTypes,
         perm: 'ui-inventory.settings.contributor-types',
@@ -59,37 +59,37 @@ const sections = [
         perm: 'ui-inventory.settings.instance-formats',
       },
       {
-        route: 'instanceNoteTypes',
+        route: 'instance-note-types',
         label: <FormattedMessage id="ui-inventory.instanceNoteTypes" />,
         component: InstanceNoteTypes,
         perm: 'ui-inventory.settings.instance-note-types',
       },
       {
-        route: 'instanceStatusTypes',
+        route: 'instance-status-types',
         label: <FormattedMessage id="ui-inventory.instanceStatusTypes" />,
         component: InstanceStatusTypes,
         perm: 'ui-inventory.settings.instance-statuses',
       },
       {
-        route: 'modesOfIssuance',
+        route: 'modes-of-issuance',
         label: <FormattedMessage id="ui-inventory.modesOfIssuance" />,
         component: ModesOfIssuance,
         perm: 'ui-inventory.settings.modes-of-issuance',
       },
       {
-        route: 'natureOfContentTerms',
+        route: 'nature-of-content-terms',
         label: <FormattedMessage id="ui-inventory.natureOfContentTerms" />,
         component: NatureOfContentTerms,
         perm: 'ui-inventory.settings.nature-of-content-terms',
       },
       {
-        route: 'identifierTypes',
+        route: 'identifier-types',
         label: <FormattedMessage id="ui-inventory.resourceIdentifierTypes" />,
         component: ResourceIdentifierTypes,
         perm: 'ui-inventory.settings.identifier-types',
       },
       {
-        route: 'resourcetypes',
+        route: 'resource-types',
         label: <FormattedMessage id="ui-inventory.resourceTypes" />,
         component: ResourceTypes,
         perm: 'ui-inventory.settings.instance-types',
@@ -100,13 +100,13 @@ const sections = [
     label: <FormattedMessage id="ui-inventory.holdings" />,
     pages: [
       {
-        route: 'holdingsNoteTypes',
+        route: 'holdings-note-types',
         label: <FormattedMessage id="ui-inventory.holdingsNoteTypes" />,
         component: HoldingsNoteTypes,
         perm: 'ui-inventory.settings.holdings-note-types',
       },
       {
-        route: 'holdingsTypes',
+        route: 'holdings-types',
         label: <FormattedMessage id="ui-inventory.holdingsTypes" />,
         component: HoldingsTypes,
         perm: 'ui-inventory.settings.holdings-types',
@@ -117,19 +117,19 @@ const sections = [
     label: <FormattedMessage id="ui-inventory.items" />,
     pages: [
       {
-        route: 'itemNoteTypes',
+        route: 'item-note-types',
         label: <FormattedMessage id="ui-inventory.itemNoteTypes" />,
         component: ItemNoteTypes,
         perm: 'ui-inventory.settings.item-note-types',
       },
       {
-        route: 'loantypes',
+        route: 'loan-types',
         label: <FormattedMessage id="ui-inventory.loanTypes" />,
         component: LoanTypes,
         perm: 'ui-inventory.settings.loantypes',
       },
       {
-        route: 'materialtypes',
+        route: 'material-types',
         label: <FormattedMessage id="ui-inventory.materialTypes" />,
         component: MaterialTypes,
         perm: 'ui-inventory.settings.materialtypes',
@@ -140,13 +140,13 @@ const sections = [
     label: <FormattedMessage id="ui-inventory.instanceHoldingsItem" />,
     pages: [
       {
-        route: 'statisticalCodeTypes',
+        route: 'statistical-code-types',
         label: <FormattedMessage id="ui-inventory.statisticalCodeTypes" />,
         component: StatisticalCodeTypes,
         perm: 'ui-inventory.settings.statistical-code-types',
       },
       {
-        route: 'URLrelationship',
+        route: 'url-relationship',
         label: <FormattedMessage id="ui-inventory.URLrelationship" />,
         component: URLRelationship,
         perm: 'ui-inventory.settings.electronic-access-relationships',
