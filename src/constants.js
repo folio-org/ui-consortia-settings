d87f1f3f296dcb3069c114c043462ae629904fca
Yury Saukou <yury_saukou@epam.com>
2023-07-18 12:17:26 +0400

    UICONSET-117 Implement usePublishCoordinator hook

--- src/constants.js
+++ src/constants.js
@@ -31,6 +31,7 @@ export const METADATA_PROVIDER_API = 'metadata-provider';
 export const MODES_OF_ISSUANCE_API = 'modes-of-issuance';
 export const NATURE_OF_CONTENT_TERMS_API = 'nature-of-content-terms';
 export const PATRON_GROUPS_API = 'groups';
+export const PUBLICATIONS_API = 'publications';
 export const STATISTICAL_CODE_TYPES_API = 'statistical-code-types';
 export const STATISTICAL_CODES_API = 'statistical-codes';
 /*  */
