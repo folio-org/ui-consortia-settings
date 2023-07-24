a03738d12f05f603cbfc9201fbef40d46d175fe9
Yury Saukou <yury_saukou@epam.com>
2023-07-24 19:03:53 +0400

    UICONSET-111 add JSDoc for 'groupMembersSettings'

--- src/routes/ConsortiumManager/utils/groupMembersSettings.js
+++ src/routes/ConsortiumManager/utils/groupMembersSettings.js
@@ -5,7 +5,24 @@ import {
   partition,
 } from 'lodash/fp';
 
-// TODO: add docs
+/**
+ * Group the list of settings into local and shared. Local, in turn, are grouped by tenant ID.
+ *
+ * Shared settings are available in each of the tenants, so they are included in the local list.
+ *
+ * @template {{
+ *  tenantId: string,
+ *  shared: boolean,
+ * }} HydratedSetting
+ *
+ * @param {HydratedSetting[]} hydratedSettings - settings hydrated with `shared` and `tenantId` values.
+ * @returns {{
+ *  shared: HydratedSetting[],
+ *  local: {
+ *    [tenantId: string]: HydratedSetting[]
+ *  }
+ * }}
+ */
 export const groupMembersSettings = (hydratedSettings) => {
   const [sharedSettings, memberSettings] = partition(({ shared }) => Boolean(shared), hydratedSettings);
 
