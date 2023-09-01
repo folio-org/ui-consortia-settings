9fee80984db2cfd731647b31a63cdbb04b027b21
Yury Saukou <yury_saukou@epam.com>
2023-09-01 13:16:17 +0400

    add unit tests

--- src/utils/EventEmitter.test.js
+++ src/utils/EventEmitter.test.js
@@ -1,37 +1,36 @@
 import { EventEmitter } from './EventEmitter';
 
+const EVENT_TYPE = 'test-event-type';
+const callback = jest.fn();
+const payload = 'Test payload';
+
 describe('EventEmitter', () => {
   let emitter;
-  let callback;
 
   beforeEach(() => {
     emitter = new EventEmitter();
-    callback = jest.fn();
-  });
-
-  afterEach(() => {
-    callback.mockReset();
+    callback.mockClear();
   });
 
   it('should add and invoke event listeners', () => {
-    emitter.on('customEvent', callback);
-    emitter.emit('customEvent', 'test data');
+    emitter.on(EVENT_TYPE, callback);
+    emitter.emit(EVENT_TYPE, payload);
 
-    expect(callback).toHaveBeenCalledWith('test data');
+    expect(callback).toHaveBeenCalledWith(expect.objectContaining({ detail: payload }));
   });
 
   it('should remove event listeners', () => {
-    emitter.on('customEvent', callback);
-    emitter.off('customEvent', callback);
-    emitter.emit('customEvent', 'test data');
+    emitter.on(EVENT_TYPE, callback);
+    emitter.off(EVENT_TYPE, callback);
+    emitter.emit(EVENT_TYPE, payload);
 
     expect(callback).not.toHaveBeenCalled();
   });
 
   it('should emit events with the correct data', () => {
-    emitter.on('customEvent', callback);
-    emitter.emit('customEvent', 'test data');
+    emitter.on(EVENT_TYPE, callback);
+    emitter.emit(EVENT_TYPE, payload);
 
-    expect(callback).toHaveBeenCalledWith('test data');
+    expect(callback).toHaveBeenCalledWith(expect.objectContaining({ detail: payload }));
   });
 });
