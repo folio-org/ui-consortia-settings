44caad65265721630ef8ce40ac1d73e197ef22b6
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2023-09-12 19:19:38 +0400

    UICONSET-139: Consortia-settings: format numbers in "totalRecords" columns

--- src/routes/ConsortiumManager/settings/data-export/utils.js
+++ src/routes/ConsortiumManager/settings/data-export/utils.js
@@ -9,7 +9,21 @@ export const getExportJobLogsListResultsFormatter = ({ intl }) => ({
   [EXPORT_JOB_LOG_COLUMNS.status]: record => intl.formatMessage({ id: `ui-data-export.jobStatus.${camelCase(record.status)}` }),
   [EXPORT_JOB_LOG_COLUMNS.runBy]: record => getFullName({ personal: record.runBy }).trim(),
   [EXPORT_JOB_LOG_COLUMNS.totalRecords]: record => intl.formatNumber(record.progress?.total),
-  [EXPORT_JOB_LOG_COLUMNS.errors]: record => record.progress?.failed || '',
+  [EXPORT_JOB_LOG_COLUMNS.errors]: record => {
+    const failedSrs = record.progress?.failed?.duplicatedSrs;
+    const failedOther = record.progress?.failed?.otherFailed;
+
+    switch (true) {
+      case failedSrs === 0 && failedOther > 0:
+        return `${failedOther}`;
+      case failedSrs > 0 && failedOther > 0:
+        return `${failedOther}, ${failedSrs} duplicates`;
+      case failedSrs > 0 && failedOther === 0:
+        return `${failedSrs} duplicates`;
+      default:
+        return '';
+    }
+  },
   [EXPORT_JOB_LOG_COLUMNS.exported]: record => record.progress?.exported || '',
 });
 
