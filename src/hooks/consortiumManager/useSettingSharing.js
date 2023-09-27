09a3b1a4a1b72263bc80dc79411aa0ab9646e658
Yury Saukou <yury_saukou@epam.com>
2023-09-27 12:10:08 +0400

    UICONSET-143 Display toast message when deletion was failed for some members

--- src/hooks/consortiumManager/useSettingSharing.js
+++ src/hooks/consortiumManager/useSettingSharing.js
@@ -87,6 +87,9 @@ export const useSettingSharing = ({ path }, options = {}) => {
       const request = {
         settingId: entry.id,
         url: path,
+        payload: {
+          ...entry,
+        },
       };
 
       return initSettingSharingRequest(request, { method: HTTP_METHODS.DELETE });
