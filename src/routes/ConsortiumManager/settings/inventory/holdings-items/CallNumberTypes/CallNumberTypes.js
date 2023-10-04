2d39389d21491c43938ebb1be4c3f6fb09b765a2
Yury Saukou <yury_saukou@epam.com>
2023-10-04 13:25:05 +0400

    UICONSET-44 Add unit test

--- src/routes/ConsortiumManager/settings/inventory/holdings-items/CallNumberTypes/CallNumberTypes.js
+++ src/routes/ConsortiumManager/settings/inventory/holdings-items/CallNumberTypes/CallNumberTypes.js
@@ -10,23 +10,23 @@ import {
 } from '../../../../constants';
 import { DEFAULT_ITEM_TEMPLATE } from '../../constants';
 
+const TRANSLATIONS = getControlledVocabTranslations('ui-consortia-settings.consortiumManager.controlledVocab.callNumberTypes');
 const FIELDS_MAP = {
   name: 'name',
   source: 'source',
   lastUpdated: 'lastUpdated',
 };
-const VISIBLE_FIELDS = Object.values(FIELDS_MAP);
-const READONLY_FIELDS = [FIELDS_MAP.source];
-const UNIQUE_FIELDS = [FIELDS_MAP.name];
-const COLUMN_MAPPING = {
-  [FIELDS_MAP.name]: <FormattedMessage id="ui-inventory.name" />,
-  [FIELDS_MAP.source]: <FormattedMessage id="ui-inventory.source" />,
-};
-const TRANSLATIONS = getControlledVocabTranslations('ui-consortia-settings.consortiumManager.controlledVocab.callNumberTypes');
 const PERMISSIONS = {
   create: 'inventory-storage.call-number-types.item.post',
   delete: 'inventory-storage.call-number-types.item.delete',
   update: 'inventory-storage.call-number-types.item.put',
+};
+const VISIBLE_FIELDS = Object.values(FIELDS_MAP);
+const UNIQUE_FIELDS = [FIELDS_MAP.name];
+const READONLY_FIELDS = [FIELDS_MAP.source];
+const COLUMN_MAPPING = {
+  [FIELDS_MAP.name]: <FormattedMessage id="ui-inventory.name" />,
+  [FIELDS_MAP.source]: <FormattedMessage id="ui-inventory.source" />,
 };
 
 export const CallNumberTypes = () => {
