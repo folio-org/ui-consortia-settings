8d08c895a9645c1f057e8f42edfb54cc63e7eb65
Yury Saukou <yury_saukou@epam.com>
2024-05-15 10:59:53 +0400

    UICONSET-178 Confirmation modal for enabling the central ordering setting across the consortium

--- src/settings/CentralOrdering/CentralOrderingForm.js
+++ src/settings/CentralOrdering/CentralOrderingForm.js
@@ -14,6 +14,8 @@ import {
   Button,
   Checkbox,
   Col,
+  ConfirmationModal,
+  MessageBanner,
   Pane,
   PaneFooter,
   PaneHeader,
@@ -24,19 +26,53 @@ import {
   useStripes,
 } from '@folio/stripes/core';
 import stripesFinalForm from '@folio/stripes/final-form';
+import { useToggle } from '@folio/stripes-acq-components';
+
+const CENTRAL_ORDERING_FIELD_NAME = 'enabled';
 
 const CentralOrderingForm = ({
+  form: { change },
   handleSubmit,
+  initialValues,
   pristine,
   submitting,
 }) => {
   const intl = useIntl();
   const paneTitleRef = useRef();
   const stripes = useStripes();
+  const confirmEnableSettingPromiseRef = useRef(Promise);
+
+  const [isConfirmEnableSettingModalOpen, toggleConfirmEnableSettingModal] = useToggle(false);
+
+  const isCentralOrderingDisabled = !stripes.hasPerm('ui-consortia-settings.settings.networkOrdering.edit') || initialValues[CENTRAL_ORDERING_FIELD_NAME];
+  const confirmCentralOrderingMessage = [
+    intl.formatMessage({ id: 'ui-consortia-settings.settings.centralOrdering.alert.message' }),
+    intl.formatMessage({ id: 'ui-consortia-settings.settings.centralOrdering.confirmModal.message' }),
+  ].join(' ');
 
   const handlePaneFocus = useCallback(() => {
     return paneTitleRef.current?.focus();
   }, []);
+
+  const handleCentralOrderingChange = useCallback(async ({ target }) => {
+    const checked = target?.checked;
+    const changeFieldValue = (value) => change(CENTRAL_ORDERING_FIELD_NAME, value);
+
+    if (checked) {
+      await new Promise((resolve, reject) => {
+        confirmEnableSettingPromiseRef.current = { resolve, reject };
+
+        toggleConfirmEnableSettingModal();
+      })
+        .then(() => {
+          changeFieldValue(true);
+          toggleConfirmEnableSettingModal();
+        })
+        .catch(toggleConfirmEnableSettingModal);
+    } else {
+      changeFieldValue(checked);
+    }
+  }, [change, toggleConfirmEnableSettingModal]);
 
   const paneFooter = useMemo(() => {
     const end = (
@@ -76,22 +112,42 @@ const CentralOrderingForm = ({
         record={intl.formatMessage({ id: 'ui-consortia-settings.settings.centralOrdering.label' })}
       />
       <Row>
+        {initialValues[CENTRAL_ORDERING_FIELD_NAME] && (
+          <Col xs={12}>
+            <MessageBanner type="warning">
+              <FormattedMessage id="ui-consortia-settings.settings.centralOrdering.alert.message" />
+            </MessageBanner>
+          </Col>
+        )}
         <Col xs={12}>
           <Field
             component={Checkbox}
             label={<FormattedMessage id="ui-consortia-settings.settings.centralOrdering.checkbox.label" />}
-            name="enabled"
+            name={CENTRAL_ORDERING_FIELD_NAME}
             type="checkbox"
-            disabled={!stripes.hasPerm('ui-consortia-settings.settings.networkOrdering.edit')}
+            disabled={isCentralOrderingDisabled}
+            onChange={handleCentralOrderingChange}
           />
         </Col>
       </Row>
+
+      <ConfirmationModal
+        id="enable-central-ordering-confirmation"
+        open={isConfirmEnableSettingModalOpen}
+        heading={intl.formatMessage({ id: 'ui-consortia-settings.settings.centralOrdering.label' })}
+        message={confirmCentralOrderingMessage}
+        confirmLabel={intl.formatMessage({ id: 'ui-consortia-settings.button.confirm' })}
+        onConfirm={confirmEnableSettingPromiseRef.current.resolve}
+        onCancel={confirmEnableSettingPromiseRef.current.reject}
+      />
     </Pane>
   );
 };
 
 CentralOrderingForm.propTypes = {
+  form: PropTypes.object.isRequired,
   handleSubmit: PropTypes.func.isRequired,
+  initialValues: PropTypes.object.isRequired,
   pristine: PropTypes.bool.isRequired,
   submitting: PropTypes.bool.isRequired,
 };
