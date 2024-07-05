18f8ceac3dfcb54f725559cce4992ebeae001e46
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2024-07-05 15:59:03 +0400

    UICONSET-163: reorder imports

--- src/hooks/useAuthorizationRoles/useAuthorizationRoles.js
+++ src/hooks/useAuthorizationRoles/useAuthorizationRoles.js
@@ -1,5 +1,5 @@
-import { useQuery } from 'react-query';
 import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 
 import {useNamespace, useOkapiKy, useStripes} from '@folio/stripes/core';
 
