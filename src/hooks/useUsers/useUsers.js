8e78292c1b378bbf1bf8d66e19e108332463151c
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2024-07-18 14:43:23 +0400

    UICONSET-167: Compare User capabilities in full screen view

---
+++ src/hooks/useUsers/useUsers.js
@@ -0,0 +1,28 @@
+import { useQuery } from 'react-query';
+
+import { useNamespace, useOkapiKy } from '@folio/stripes/core';
+import { useEffect, useState } from 'react';
+
+export const useUsers = (tenant = '') => {
+  const [namespaceKey] = useNamespace({ key: 'relatedUsers' });
+  const ky = useOkapiKy({ tenant });
+  const [users, setUsers] = useState([]);
+
+  const { data, isFetching, isSuccess } = useQuery(
+    {
+      queryKey: [namespaceKey, tenant],
+      queryFn: () => ky.get('users').json(),
+    },
+  );
+
+  useEffect(() => {
+    if (isSuccess && data?.users) {
+      setUsers(data.users);
+    }
+  }, [data, isSuccess]);
+
+  return {
+    users,
+    isFetching,
+  };
+};
