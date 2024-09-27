e8510c33415fe5b878c5ac98a54bd4333cf7feba
Yury Saukou <yury_saukou@epam.com>
2024-09-27 17:27:34 +0400

    display role type in the list columns

--- src/routes/ConsortiumManager/settings/authorization-roles/constants.js
+++ src/routes/ConsortiumManager/settings/authorization-roles/constants.js
@@ -3,20 +3,23 @@ import { FormattedMessage } from 'react-intl';
 export const COLUMN_NAMES = {
   description: 'description',
   name: 'name',
+  type: 'type',
   updated: 'updated',
   updatedBy: 'updatedBy',
 };
 
 export const COLUMN_MAPPING = {
-  [COLUMN_NAMES.name]: <FormattedMessage id="ui-authorization-roles.columns.name" />,
-  [COLUMN_NAMES.description]: <FormattedMessage id="ui-authorization-roles.columns.description" />,
-  [COLUMN_NAMES.updated]: <FormattedMessage id="ui-authorization-roles.columns.updatedDate" />,
-  [COLUMN_NAMES.updatedBy]: <FormattedMessage id="ui-authorization-roles.columns.updatedBy" />,
+  [COLUMN_NAMES.name]: <FormattedMessage id="stripes-authorization-components.columns.name" />,
+  [COLUMN_NAMES.description]: <FormattedMessage id="stripes-authorization-components.columns.description" />,
+  [COLUMN_NAMES.type]: <FormattedMessage id="stripes-authorization-components.columns.type" />,
+  [COLUMN_NAMES.updated]: <FormattedMessage id="stripes-authorization-components.columns.updatedDate" />,
+  [COLUMN_NAMES.updatedBy]: <FormattedMessage id="stripes-authorization-components.columns.updatedBy" />,
 };
 
 export const VISIBLE_COLUMNS = [
   COLUMN_NAMES.name,
   COLUMN_NAMES.description,
+  COLUMN_NAMES.type,
   COLUMN_NAMES.updated,
   COLUMN_NAMES.updatedBy,
 ];
