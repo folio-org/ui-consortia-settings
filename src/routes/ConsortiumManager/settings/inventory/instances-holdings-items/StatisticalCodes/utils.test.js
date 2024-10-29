86a1f537b531b860a5273b6ab1470007a3317fcf
Alisher Musurmonov <alisher_musurmonov1@epam.com>
2024-10-29 18:08:58 +0500

    test: add test coverage

--- src/routes/ConsortiumManager/settings/inventory/instances-holdings-items/StatisticalCodes/utils.test.js
+++ src/routes/ConsortiumManager/settings/inventory/instances-holdings-items/StatisticalCodes/utils.test.js
@@ -1,19 +1,33 @@
-import { getCurrentStatisticalCodeType } from "./utils";
+import { getCurrentStatisticalCodeType, getStatisticalCodeTypeOptions } from './utils';
+
+const localStatisticalCodeTypes = [
+  { id: '1', name: 'name1' },
+  { id: '2', name: 'name2' },
+];
+const sharedStatisticalCodeTypes = [
+  { id: '3', name: 'name3' },
+  { id: '4', name: 'name4' },
+];
+
+const codeTypes = {
+  local: { 'tenantId': localStatisticalCodeTypes },
+  shared: sharedStatisticalCodeTypes,
+};
 
 describe('getCurrentStatisticalCodeType', () => {
+  it('should return undefined when statisticalCodeTypeId is not provided', () => {
+    const shared = false;
+    const tenantId = 'tenantId';
+
+    const result = getCurrentStatisticalCodeType({
+      shared,
+      tenantId,
+    });
+
+    expect(result).toBeUndefined();
+  });
+
   it('should return current statistical code type', () => {
-    const codeTypes = {
-      local: {
-        'tenantId': [
-          { id: '1', name: 'name1' },
-          { id: '2', name: 'name2' },
-        ]
-      },
-      shared: [
-        { id: '3', name: 'name3' },
-        { id: '4', name: 'name4' },
-      ]
-    };
     const statisticalCodeTypeId = '2';
     const shared = false;
     const tenantId = 'tenantId';
@@ -29,18 +43,6 @@ describe('getCurrentStatisticalCodeType', () => {
   });
 
   it('should return current statistical code type when shared is true', () => {
-    const codeTypes = {
-      local: {
-        'tenantId': [
-          { id: '1', name: 'name1' },
-          { id: '2', name: 'name2' },
-        ]
-      },
-      shared: [
-        { id: '3', name: 'name3' },
-        { id: '4', name: 'name4' },
-      ]
-    };
     const statisticalCodeTypeId = '3';
     const shared = true;
     const tenantId = 'tenantId';
@@ -53,5 +55,27 @@ describe('getCurrentStatisticalCodeType', () => {
     });
 
     expect(result).toEqual({ id: '3', name: 'name3' });
+  });
+});
+
+describe('getStatisticalCodeTypeOptions', () => {
+  it('should return statistical code type options', () => {
+    const groupedStatisticalCodeTypes = codeTypes;
+    const statisticalCodeTypeId = '2';
+    const shared = false;
+    const tenantId = 'tenantId';
+
+    const result = getStatisticalCodeTypeOptions({
+      shared,
+      tenantId,
+      statisticalCodeTypeId,
+      groupedStatisticalCodeTypes,
+    });
+
+    expect(result).toEqual([
+      { label: 'name2', value: '2' },
+      { label: 'name3', value: '3' },
+      { label: 'name4', value: '4' },
+    ]);
   });
 });
