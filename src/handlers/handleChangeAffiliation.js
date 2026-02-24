5a1b891905ff79e1c00e44ed3da7035ac3aef5f7
Zak Burke <zburke@ebsco.com>
2026-02-24 07:10:46 -0500

    actually rename the function, as the original comment said

--- src/handlers/handleChangeAffiliation.js
+++ src/handlers/handleChangeAffiliation.js
@@ -1,3 +1,3 @@
 import { SwitchActiveAffiliation } from '../components';
 
-export const handleChangeAffilication = () => SwitchActiveAffiliation;
+export const handleChangeAffiliation = () => SwitchActiveAffiliation;
